0b4a9d9f9d526b3a10bc777cde3118c332fa422c
matheustakasaki <matheus8antunes@gmail.com>
2021-01-10 23:26:15 -0300

    first commit

---
+++ aluno.js
@@ -0,0 +1,7 @@
+export class Aluno {
+    constructor (nome, cpf, matricula){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.matricula = matricula;
+    }
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import { Aluno } from "./aluno.js";
+
+const aluno= new Aluno("José Henrique", "131.050.069-07", 123456)
+
+console.log(aluno);
